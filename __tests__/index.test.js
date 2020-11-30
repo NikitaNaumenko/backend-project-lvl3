485458d59338bda6ad8b8094f81bd4cbf70e538a
Nikita Naumenko <naumenkoniki@gmail.com>
2020-11-30 17:05:54 +0300

    add simple test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,8 @@ import nock from 'nock';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import pageLoader from '../index.js';
-import fs from 'fs';
+import { promises as fs } from 'fs';
+import os from 'os';
 
 /* eslint-disable no-underscore-dangle */
 const __filename = fileURLToPath(import.meta.url);
@@ -10,16 +11,20 @@ const __dirname = dirname(__filename);
 /* eslint-enable no-underscore-dangle */
 
 const resolvePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
-const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
+const readFile = async (filepath) => await fs.readFile(filepath, 'utf8');
+const htmlResult = await readFile(resolvePath('index.html'))
 
-const htmlResult = readFile(resolvePath('index.html'))
-
-nock('https://hexlet.io')
-  .get('/courses')
-  .reply(200, htmlResult )
-
-test('index', async () => {
-  pageLoader()
-
+let dirPath;
+beforeEach(async () => {
+  dirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 })
 
+
+test('index', async () => {
+  nock('https://hexlet.io')
+    .get('/courses')
+    .reply(200, htmlResult )
+
+  const result = await pageLoader('https://hexlet.io', dirPath)
+  expect(result).toEqual(htmlResult)
+})
---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import pageLoader from '../index.js'
+
+program
+  .option('-o, --output <dir>', 'path to uploaded path', process.cwd())
+  .arguments('<url>')
+  .action(async (url, cmdObj) => {
+    try {
+      await pageLoader(url, cmdObj.dir);
+    } catch {
+      console.log('Error')
+      process.exitCode = 1
+    }
+  })
+  .parse(process.argv);
+
