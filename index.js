34248efbc6cde585491467239ab3ccfcada3951a
Nikita Naumenko <naumenkoniki@gmail.com>
2020-12-02 09:27:29 +0300

    processed urls

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import path from 'path';
 import _ from 'lodash';
+import $ from 'cheerio';
 import { promises as fs } from 'fs';
 
-const generateSlug = (pageUrl) => {
-  const url = new URL(pageUrl);
+const generateSlug = (url) => {
   return [...url.host.split('.'), ..._.compact(url.pathname.split('/'))].join('-');
 };
 
@@ -14,11 +14,24 @@ const download = (urlString) => axios.get(urlString, { responseType: 'arraybuffe
     throw new Error(`Error while downloading page "${urlString}"\nReason - "${message}"`);
   });
 
+const buildAssetUrl = (assetPath, origin) => new URL(assetPath, origin);
+
+const prepareAssetsUrls = (html, assetsPath, origin) => {
+  const links = $('img', html).toArray().map((elem) => $(elem).attr('src'));
+  const assetsUrl = links.reduce((link) => {...acc, [link]: buildAssetUrl(link, origin) }, {});
+  return assetsUrl;
+};
+
+const replaceUrls = (html, urls) => {
+  $('img', html).map((elem))
+}
+
 export default (url, outputDir) => {
   console.log(`Passed url: ${url}`);
   console.log(`Passed outputDir: ${outputDir}`);
 
-  const slug = generateSlug(url);
+  const pageUrl = new URL(url);
+  const slug = generateSlug(pageUrl);
   const pageFile = `${slug}.html`;
   const pageFilePath = path.resolve(outputDir, pageFile);
   console.log(`Page file ${pageFile} is located: ${pageFilePath}`);
@@ -28,6 +41,7 @@ export default (url, outputDir) => {
   console.log(`Assets dir is ${assetsDir} is located: ${assetsDirPath}`);
 
   return download(url)
-    .then((data) => fs.writeFile(pageFilePath, data))
-    .then(() => fs.readFile(pageFilePath, 'utf-8'));
+    .then((data) => prepareAssets(data, assetsDirPath, pageUrl.origin))
+    // .then((data) => fs.writeFile(pageFilePath, data))
+    // .then(() => fs.readFile(pageFilePath, 'utf-8'));
 };
