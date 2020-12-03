bf8b442095c7305aa010020329bf24fa95dacdb5
Nikita Naumenko <naumenkoniki@gmail.com>
2020-12-03 20:11:17 +0300

    complete second step

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,20 +10,33 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 /* eslint-enable no-underscore-dangle */
 
-const resolvePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const resolvePath = (...paths) => path.join(__dirname, '..', '__fixtures__', ...paths);
+
+nock.disableNetConnect();
+const scope = nock('https://hexlet.io').persist();
 
 let dirPath;
 beforeEach(async () => {
   dirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test('index', async () => {
-  const htmlResult = await fs.readFile(resolvePath('index.html'), 'utf-8');
+describe('positive', () => {
+  test('download page', async () => {
+    const htmlResponse = await fs.readFile(resolvePath('index.html'), 'utf-8');
+    // console.log(resolvePath('hexlet-io-courses.html'))
+    // console.log(resolvePath('hexlet-io-courses_files'))
+    // console.log(resolvePath('hexlet-io-courses_files', 'nodejs.png'))
+    scope
+      .get('/courses')
+      .reply(200, htmlResponse);
 
-  nock('https://hexlet.io')
-    .get('/courses')
-    .reply(200, htmlResult);
+    scope
+      .get('/assets/professions/nodejs.png')
+      .replyWithFile(200, resolvePath('expected', 'hexlet-io-courses_files', 'assets-professions-nodejs.png'));
 
-  const result = await pageLoader('https://hexlet.io/courses', dirPath);
-  expect(result).toEqual(htmlResult);
+    const expected = await fs.readFile(resolvePath('expected', 'hexlet-io-courses.html'), 'utf-8');
+
+    const result = await pageLoader('https://hexlet.io/courses', dirPath);
+    expect(result).toEqual(expected);
+  });
 });
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 import path from 'path';
 import _ from 'lodash';
 import $ from 'cheerio';
+import debug from 'debug';
 import { promises as fs } from 'fs';
 
-const generateSlug = (url) => {
-  return [...url.host.split('.'), ..._.compact(url.pathname.split('/'))].join('-');
-};
+const log = debug('page-loader');
+
+const generateSlug = (url) => [...url.host.split('.'), ..._.compact(url.pathname.split('/'))].join('-');
 
 const download = (urlString) => axios.get(urlString, { responseType: 'arraybuffer' })
   .then(({ data }) => data.toString())
@@ -16,32 +17,55 @@ const download = (urlString) => axios.get(urlString, { responseType: 'arraybuffe
 
 const buildAssetUrl = (assetPath, origin) => new URL(assetPath, origin);
 
-const prepareAssetsUrls = (html, assetsPath, origin) => {
+const prepareAssetsInfo = (html, assetsPath, origin) => {
   const links = $('img', html).toArray().map((elem) => $(elem).attr('src'));
-  const assetsUrl = links.reduce((link) => {...acc, [link]: buildAssetUrl(link, origin) }, {});
-  return assetsUrl;
+
+  return links.map((link) => ({
+    url: buildAssetUrl(link, origin),
+    filepath: path.join(assetsPath, _.trim(link, '/').replace(/\//g, '-')),
+    key: link,
+  }));
 };
 
-const replaceUrls = (html, urls) => {
-  $('img', html).map((elem))
-}
+const prepareAssets = (initialHtml, assetsDirPath, origin) => {
+  const assetsInfo = prepareAssetsInfo(initialHtml, assetsDirPath, origin);
+  const updatedHtml = assetsInfo.reduce((html, elem) => {
+    const $$ = $.load(html, { decodeEntities: false });
+    $$(`img[src='${elem.key}']`).attr('src', elem.filepath);
+    return $$.html();
+  }, initialHtml);
+  return { assetsInfo, updatedHtml };
+};
+
+const createDir = (dirpath) => fs.mkdir(dirpath);
 
 export default (url, outputDir) => {
-  console.log(`Passed url: ${url}`);
-  console.log(`Passed outputDir: ${outputDir}`);
+  log('Inital data ', { url, outputDir });
 
   const pageUrl = new URL(url);
   const slug = generateSlug(pageUrl);
   const pageFile = `${slug}.html`;
   const pageFilePath = path.resolve(outputDir, pageFile);
-  console.log(`Page file ${pageFile} is located: ${pageFilePath}`);
+
+  log(`Page file ${pageFile} is located: ${pageFilePath}`);
 
   const assetsDir = `${slug}_files`;
   const assetsDirPath = path.resolve(outputDir, assetsDir);
-  console.log(`Assets dir is ${assetsDir} is located: ${assetsDirPath}`);
 
   return download(url)
-    .then((data) => prepareAssets(data, assetsDirPath, pageUrl.origin))
-    // .then((data) => fs.writeFile(pageFilePath, data))
-    // .then(() => fs.readFile(pageFilePath, 'utf-8'));
+    .then((data) => {
+      log('Create assets directory: ', { assetsDirPath });
+      return createDir(assetsDirPath).then(() => data);
+    })
+    .then((data) => {
+      log('Process assets');
+      const { assetsInfo, updatedHtml } = prepareAssets(data, assetsDir, pageUrl.origin);
+      assetsInfo.map(({ filepath, url: assetUrl }) => {
+        log(`Download: ${assetUrl} into ${filepath}`);
+
+        return download(assetUrl.toString()).then((content) => fs.writeFile(path.resolve(assetsDirPath, filepath), Buffer.from(content, 'base64')));
+      });
+
+      return fs.writeFile(pageFilePath, updatedHtml);
+    }).then(() => fs.readFile(pageFilePath, 'utf-8'));
 };
