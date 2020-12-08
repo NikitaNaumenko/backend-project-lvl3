24afe8ee31126f39b24df5cb005e36063a5950d7
Nikita Naumenko <naumenkoniki@gmail.com>
2020-12-08 18:24:02 +0300

    fix

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ import pageLoader from '../index.js';
 program
   .option('-o, --output <dir>', 'path to uploaded path', process.cwd())
   .arguments('<url>')
-  .action(async (url, cmdObj) => pageLoader(url, cmdObj.output, 'default')
+  .action(async (url, cmdObj) => pageLoader(url, cmdObj.output)
     .then(() => console.log('Page was loaded'))
     .catch((err) => {
       console.error(err.toString());
